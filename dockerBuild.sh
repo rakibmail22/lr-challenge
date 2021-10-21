12c02417ff3578ef09c0e45776254bea458680b0
Bashir Ahmed Rakib <rakibmail22@gmail.com>
2021-10-21 15:35:39 +0600

    Fixed dockerBuild script to use gradle wrapper

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-gradle clean build
+./gradlew clean build
 mkdir -p build/dependency && (cd build/dependency; jar -xf ../libs/challenge-0.0.1-SNAPSHOT.jar)
 docker build -t lforward/lr-challenge .
